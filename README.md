5c90212810b4a52544ac773ed9e914bb9a766ebf
novice1011 <mqomaruzzaman@gmail.com>
2019-10-30 21:21:32 +0800

    all works
    *with nonblocking read
    
    issue: nonblocking read will break program if it gets nothing

---
+++ README.md
@@ -0,0 +1,3 @@
+# UDP client using c++
+
+
--- main.cpp
+++ main.cpp
@@ -14,8 +14,6 @@
 // Driver code
 int main() {
     int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from client";
     struct sockaddr_in	 servaddr;
 
     // Creating socket file descriptor
@@ -24,26 +22,50 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    //set reading timeout
+    struct timeval tv;
+    tv.tv_sec = 2;
+    tv.tv_usec = 0;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
+        perror("timeout setting error");
+        return 0;
+    }
+
     memset(&servaddr, 0, sizeof(servaddr));
 
     // Filling server information
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_addr.s_addr =INADDR_ANY;// inet_addr("10.42.0.100");//("192.168.4.1") or INADDR_ANY
 
-    unsigned int n, len;
+    int msgNum=0;
+    while (1){
+        unsigned int n, len;
+        char buffer[MAXLINE];
 
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-            sizeof(servaddr));
-    printf("Hello message sent.\n");
+        char sendlalala[100];
+        sprintf(sendlalala,"This is number %d\n",msgNum);
 
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
+        sendto(sockfd, (const char *)sendlalala, strlen(sendlalala),
+               MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+               sizeof(servaddr));
+        printf("%s", sendlalala);
+
+        msgNum++;
+        if (msgNum>100)msgNum=0;
+
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                     MSG_WAITALL, (struct sockaddr *) &servaddr,
+                     &len);
+        buffer[n] = '\0';
+        printf("Server : %s\n", buffer);
+
+        sleep(1);//beware, this will stop all thread
+    }
 
     close(sockfd);
     return 0;
 }
+
+//Code: https://www.geeksforgeeks.org/udp-server-client-implementation-c/
+//timeout: https://stackoverflow.com/questions/13547721/udp-socket-set-timeout
